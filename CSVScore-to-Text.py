0eaa2a06321fae40603afeb6b8b55108e6c9f0d6
Aaron Cheung <21007144+AaronCheung430@users.noreply.github.com>
2023-03-18 08:21:03 +0000

    completed csv state issue score to text python code

---
+++ CSVScore-to-Text.py
@@ -0,0 +1,43 @@
+
+# Python program to read CSV file line by line
+# import necessary packages
+import csv
+import json
+
+data = []
+weight = 10
+pk = 255
+
+with open('State_PKs_Score.csv') as file_obj:
+
+    # Create reader object by passing the file
+    # object to reader method
+    reader_obj = csv.reader(file_obj)
+    # Skip header row
+    next(reader_obj)
+
+    # Iterate over each row in the csv
+    # file using reader object
+    for row in reader_obj:
+        state_num = row[2]
+
+        for issue in range(6, 11):
+
+            issue_score = row[issue-2]
+            print(issue_score)
+            pk += 1
+
+            state_issue = {"model": "campaign_trail.state_issue_score", "pk": int(pk), "fields": {"state": int(state_num), "issue": int(issue), "state_issue_score": float(issue_score), "weight": int(weight)}}
+
+            data.append(state_issue)
+
+
+json_str = json.dumps(data)
+json_str = json_str.replace('"', '\\"')
+output_str = f'campaignTrail_temp.state_issue_score_json = JSON.parse(\"{json_str}\");'
+
+f = open("state_issue_score_json_code.txt", "w")
+f.write(output_str)
+f.close()
+
+print("State issue score json for code 2 output successfully")
